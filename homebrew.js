ee40ad9704895a59541fcf9c007eaaf655fe832a
doeringi <doeringi@hs-pforzheim.de>
2020-04-15 15:58:10 +0800

    brewtool is fully functional

--- homebrew.js
+++ homebrew.js
@@ -1,23 +1,44 @@
 $(document).ready(function() {
+  $("#recipe").hide();
   $("#hello-world").click(function(){
     /*alert("Test:" + $("#select-brewmethod").val() + $("#coffeeSize").val());*/
     var coffeeSize = parseInt($("#coffeeSize").val());
     var brewmethod =  $("#select-brewmethod").val();
+    var grSize = ["fine", "medium", "medium-coarse", "coarse"];
+    var waterTemp = ["94 Degree Celsius", "96 Degree Celsius", "97 Degree Celsius"];
+    var brewTime = ["3 min", "4 min", "5 min"];
+    console.log(grSize[0]);
+
+    $("#recipe").show();
 
     switch ($("#select-brewmethod").val()) {
       case "french-press":
-        var solution = coffeeSize/15;
+        var solution = Math.round((coffeeSize/15)*100) / 100;
         $("#solution").text(solution);
+        $("#grSize").text(grSize[3]);
+        $("#wateramount").text(coffeeSize + " ml");
+        $("#watertemp").text(waterTemp[2]);
+        $("#brewtime").text(brewTime[1]);
+        console.log(solution);
         break;
       case "chemex":
-        var solution = coffeeSize/16;
+        var solution = Math.round((coffeeSize/16)*100) / 100;
         $("#solution").text(solution);
+        $("#grSize").text(grSize[2]);
+        $("#wateramount").text(coffeeSize + " ml");
+        $("#watertemp").text(waterTemp[1]);
+        $("#brewtime").text(brewTime[1]);
         break;
       case "v60":
-        var solution = coffeeSize/14;
+        var solution = Math.round((coffeeSize/14)*100) / 100;
         $("#solution").text(solution);
+        $("#grSize").text(grSize[2]);
+        $("#wateramount").text(coffeeSize + " ml");
+        $("#watertemp").text(waterTemp[0]);
+        $("#brewtime").text(brewTime[0]);
         break;
       default:
+
 
     };
 
